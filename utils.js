ba3a16c83946aff003326299336dd36c3b6f8569
ben <benjamin020497@gmail.com>
2022-07-04 16:11:36 +0100

    Added basic collision detection

--- utils.js
+++ utils.js
@@ -20,3 +20,18 @@ function getIntersection(A, B, C, D) {
   }
   return null;
 }
+
+function polysIntersect(polygon1, polygon2) {
+  for (let i = 0; i < polygon1.length; i++) {
+    for (let j = 0; j < polygon2.length; j++) {
+      const intersect = getIntersection(
+        polygon1[i],
+        polygon1[(i + 1) % polygon1.length],
+        polygon2[j],
+        polygon2[(j + 1) % polygon2.length]
+      );
+      if (intersect) return true;
+    }
+  }
+  return false;
+}
