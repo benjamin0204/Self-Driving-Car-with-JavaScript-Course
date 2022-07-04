18ce91634e4f6aa1e11ba736d0d998e66293fccc
ben <benjamin020497@gmail.com>
2022-07-04 15:07:15 +0100

    small refactor

--- sensor.js
+++ sensor.js
@@ -8,24 +8,40 @@ class Sensor {
     this.rays = [];
   }
   update() {
+    this.#castRays();
+  }
+  #castRays() {
     this.rays = [];
     for (let i = 0; i < this.rayCount; i++) {
-      // Adding the car angle makes the sensors point the same way as the car
-      const rayAngle =
-        lerp(
-          this.raySpread / 2,
-          -this.raySpread / 2,
-          this.rayCount == 1 ? 0.5 : i / (this.rayCount - 1)
-        ) + this.car.angle;
-
-      const startPoint = { x: this.car.x, y: this.car.y };
-      const endPoint = {
-        x: this.car.x - Math.sin(rayAngle) * this.rayLength,
-        y: this.car.y - Math.cos(rayAngle) * this.rayLength,
-      };
+      const rayAngle = this.#getRayAngle(i);
+      const startPoint = this.#getStartPoint();
+      const endPoint = this.#getEndPoint(rayAngle, this.rayLength);
       this.rays.push([startPoint, endPoint]);
     }
   }
+
+  #getRayAngle(i) {
+    // Adding the car angle makes the sensors point the same way as the car
+    return (
+      lerp(
+        this.raySpread / 2,
+        -this.raySpread / 2,
+        this.rayCount == 1 ? 0.5 : i / (this.rayCount - 1)
+      ) + this.car.angle
+    );
+  }
+
+  #getStartPoint() {
+    return { x: this.car.x, y: this.car.y };
+  }
+
+  #getEndPoint(rayAngle, rayLength) {
+    return {
+      x: this.car.x - Math.sin(rayAngle) * rayLength,
+      y: this.car.y - Math.cos(rayAngle) * rayLength,
+    };
+  }
+
   draw(ctx) {
     for (let i = 0; i < this.rayCount; i++) {
       ctx.beginPath();
