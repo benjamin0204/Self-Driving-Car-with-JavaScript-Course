087f149b52cd872a2deacb3313be853c7263eed4
ben <benjamin020497@gmail.com>
2022-07-04 13:06:10 +0100

    simple Road class

--- main.js
+++ main.js
@@ -4,14 +4,15 @@ canvas.width = 200;
 // drawing context
 const ctx = canvas.getContext("2d");
 
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
 const car = new Car(100, 100, 20, 50);
-car.draw(ctx);
 
 animate();
 
 function animate() {
   car.update();
   canvas.height = window.innerHeight;
+  road.draw(ctx);
   car.draw(ctx);
   requestAnimationFrame(animate);
 }
